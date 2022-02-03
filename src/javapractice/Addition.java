37544a010260c24a6efca51e0088896a024225f7
kumudinidesale <kdesale1211@gmail.com>
2022-02-03 19:08:30 +0530

    added:addition two number

---
+++ src/javapractice/Addition.java
@@ -0,0 +1,12 @@
+package javapractice;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		int sum = a+b;
+		System.out.println("Addition is:" +sum);
+	}
+
+}
